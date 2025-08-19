829e4e3e2595e24df3f0c99cb80d7364f37d6d7b
Robert Lam <robert.lam22@gmail.com>
2025-08-19 17:56:59 +1000

    Home Page + Font import

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+export function Home() {
+    return (
+        <>
+            <h1>Quizza</h1>
+            <p>What Do You Know? Do You Know Things? Let's Find Out!</p>
+            <button className="button">Find out</button>
+        </>
+    );
+}
