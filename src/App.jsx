e8249978e52695dfec4c611adb74c8e3ae71389b
Robert Lam <robert.lam22@gmail.com>
2025-08-19 18:17:57 +1000

    Home CSS and Quiz
    State to conditionally render between 2 components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react';
 import { Home } from './components/Home';
+import { Quiz } from './components/Quiz';
 import './App.css';
 
 function App() {
+    const [start, setStart] = useState(false);
+
+    function startQuiz() {
+        setStart((prevState) => !prevState);
+        console.log(start);
+    }
+
     return (
         <section className="page">
-            <Home />
+            {start ? <Home onClick={startQuiz} /> : <Quiz />}
         </section>
     );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
-export function Home() {
+export function Home(props) {
     return (
         <>
             <h1>Quizza</h1>
             <p>What Do You Know? Do You Know Things? Let's Find Out!</p>
-            <button className="button">Find out</button>
+            <button onClick={props.onClick} className="button">
+                Find out
+            </button>
         </>
     );
 }
