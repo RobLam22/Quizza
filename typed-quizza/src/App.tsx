1c0314df76aa657edd9ad69d8df2306a9a70e39f
Robert Lam <robert.lam22@gmail.com>
2025-11-05 15:21:13 +1100

    Dashboard  & routes

--- typed-quizza/src/App.tsx
+++ typed-quizza/src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router.tsx';
+import { AuthContextProvider } from './context/AuthContext.jsx';
 
 export default function App() {
-    return <RouterProvider router={router} />;
+    return (
+        <AuthContextProvider>
+            <RouterProvider router={router} />
+        </AuthContextProvider>
+    );
 }
